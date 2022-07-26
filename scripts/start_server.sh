b93c10b8ed2ee8ad98ec1f6d43f39af8ad5de561
Wojtek Wydmuch <33324211+wojtekw0703@users.noreply.github.com>
2022-07-26 18:53:25 +0200

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo service supervisor restart
+sudo kill -s SIGTERM $(sudo supervisorctl pid)
 cd /home/ubuntu/ServiceNow-simulator/startpage
 source environment/bin/activate
 supervisord -c /home/ubuntu/ServiceNow-simulator/supervisord.conf
