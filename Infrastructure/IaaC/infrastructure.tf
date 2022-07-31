860e0c6fcc3840facef06063c33a0afe1d287563
Wojciech Wydmuch <wojciechwydmuch73@gmail.com>
2022-07-31 05:10:54 +0200

    Deleted CloudFormation code

--- Infrastructure/IaaC/infrastructure.tf
+++ Infrastructure/IaaC/infrastructure.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "XXXXXXXXXXX"
-  secret_key = "XXXXXXXXXXX"
+  access_key = "AKIAQMNBOBOR2W76GH6X"
+  secret_key = "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"
   region     = "us-east-1"
 }
 
@@ -15,7 +15,7 @@ resource "aws_instance" "ServiceNow-simulator-production" {
 # ---------------------------------------</Production>-------------------------------------#
 
 # ---------------------------------------<Monitoring>--------------------------------------#
-resource "aws_instance" "ServiceNow-simulator-monitoring" {
+resource "aws_instance" "ServiceNow-simulator-mon" {
   ami             = "ami-04505e74c0741db8d"
   instance_type   = "t2.micro"
   key_name        = aws_key_pair.key.key_name
@@ -25,7 +25,7 @@ resource "aws_instance" "ServiceNow-simulator-monitoring" {
 
 resource "aws_key_pair" "key" {
   key_name   = "servicenow-simulator"
-  public_key = "XXXXXXXXXXXXXXXXXXXXXXx"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCZkn/ncB5YVQMSCLjMcpgfpTVCNTkdFAAz5oPSpXDkQLJzN+mFIroOchoNnul7BFFqwaOUirJu6rma7oIc1hH+4472HqdqpM01zhv329LsSXx2B70tQ7b1hCTg9eljVzVowmO8w+442e1wzeWktqzXp6BxPgoBYPkjKRKKUoKBwhIeCzV+tlECE3Ijt7p7Y6CQp3RTvnxJsM0meS8VchvB1daEpvxPJ7s2pgr+Vuhlj0y4wK6xMbEJJspU8Zf4qDb9q+A7mhPbuBih0NsLcqd6KXQLgbKxLpu8rT5NduQyeepIN9sD4Y9eDo7/ko3SaTU3MhZoxq+CR985w1Vddsqx rsa-key-20220715"
 }
 
 resource "aws_security_group" "security_group_servicenow" {
